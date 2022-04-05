71451e27cb16513fff90a5421f23aeba45bdec56
Valdir Silva <valdirpiresba@gamil.com>
2022-04-05 00:44:29 -0300

    cria um server com express

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+
+import  express, { Request, Response, NextFunction } from 'express';
+
+const app = express();
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+   res.status(200).send({
+       foo: 'bar'
+   });
+});
+
+app.listen(3000, () => {
+    console.log('App started');
+})
