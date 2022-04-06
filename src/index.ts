97d06e2f889107b0fc6bc27061af0d4dc67529ad
Valdir Silva <valdirpiresba@gmail.com>
2022-04-06 00:59:17 -0300

    refactorando a rota status

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,14 @@
 
 import  express, { Request, Response, NextFunction } from 'express';
 import usersRoute from './routes/users.route'; 
+import statusRoute from './routes/status.route';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(usersRoute);
-
-app.get('/status', (req: Request, res: Response, next: NextFunction) => {
-   res.status(200).send({
-       foo: 'bar'
-   });
-});
+app.use(statusRoute);
 
 app.listen(3000, () => {
     console.log('App started');
