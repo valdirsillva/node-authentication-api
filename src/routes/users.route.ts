e1be07455d6d7e67ba6527a8fa6949d96e7942b9
Valdir Silva <valdirpiresba@gmail.com>
2022-04-06 00:24:18 -0300

    adiciona http-status-codes

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,17 +1,18 @@
 import { Router, Response, Request, NextFunction } from 'express';
+import { StatusCodes }  from 'http-status-codes';
 
 const usersRoute = Router();
 
 usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
     
     const users = [{ userName: 'Valdir' }];
-    res.status(200).send(users)
+    res.status(StatusCodes.OK).send(users)
 });
 
 usersRoute.get('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
 
     const uuid = req.params.uuid;
-    res.status(200).send({uuid});
+    res.status(StatusCodes.OK).send({uuid});
 })
 
 
