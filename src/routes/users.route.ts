1b44c85fe4cbae570a121ce21110c439228d4462
Valdir Silva <valdirpiresba@gmail.com>
2022-04-06 00:14:49 -0300

    cria rota para retornar um usuario pelo uuid

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -2,13 +2,17 @@ import { Router, Response, Request, NextFunction } from 'express';
 
 const usersRoute = Router();
 
-usersRoute.get('/users', (req: Request, res: Response, next: NextFunction ) => {
+usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
+    
     const users = [{ userName: 'Valdir' }];
-
     res.status(200).send(users)
 });
 
+usersRoute.get('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
 
+    const uuid = req.params.uuid;
+    res.status(200).send({uuid});
+})
 
 
 export default usersRoute;
