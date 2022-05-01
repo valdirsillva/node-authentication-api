fc3ee0c8051bc5169fe1f2b2b48944b9818277ab
Valdir Silva <valdirpiresba@gmail.com>
2022-04-30 21:35:43 -0300

    criando um novo usuario

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -15,9 +15,12 @@ usersRoute.get('/users/:uuid', async (req: Request<{ uuid: string }>, res: Respo
     res.status(StatusCodes.OK).send(user);
 })
 
-usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) => { 
+usersRoute.post('/users', async (req: Request, res: Response, next: NextFunction) => { 
     const newUser = req.body;
-    res.status(StatusCodes.CREATED).send(newUser);
+
+    const uuid = await userRepository.create(newUser);
+
+    res.status(StatusCodes.CREATED).send(uuid);
 });
 
 usersRoute.put('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
